d907181d86d4d8502d8749e473ea8533d07f6913
Linmic <linmic_lin@htc.com>
2012-12-06 18:25:41 +0800

    draft

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict';
+$(function() {
+    var childTpl = Handlebars.compile('');
+    var data = {
+        name: 'ddio',
+        sex: '男',
+        avatar: 'http://www.missingkids.org.tw/miss_focusimages/98253_s.jpg',
+        currentAge: 20,
+        missingAge: 8,
+        missingDate: '民國77年1月',
+        missingRegion: '新北市板橋區',
+        missingCause: '被拐(抱)走 ',
+        missingLocation: '街道',
+        character: '眉粗、眼大'
+    };
+    $('#wrapper').html( childTpl(data) );
+});
